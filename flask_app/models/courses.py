497aaee2eacf16e02a74535fd1b74456c606c7a8
Juan Camilo Duque Gil <kcamilito@gmail.com>
2022-09-11 09:41:53 -0500

    Add records model and modify category model

--- flask_app/models/courses.py
+++ flask_app/models/courses.py
@@ -3,7 +3,10 @@ from flask_app.config.mysqlconnection import connectToMySQL
 class Course:
     def __init__(self, data):
         self.id = data['id']
-        self.title = data['title']
+        self.title = data['name']
+        self.description = data['description']
+        self.description = data['instructor_id']
+        self.description = data['category_id']
         self.description = data['description']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
@@ -17,17 +20,17 @@ class Course:
         return courses
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO courses (title, description) VALUES (%(title)s, %(description)s);"
+        query = "INSERT INTO courses (name, description, instructor_id, category_id) VALUES (%(name)s, %(description)s, %(instructor_id)s,%(category_id)s);"
         return connectToMySQL('learn_app').query_db(query, data)
     @classmethod
     def get_by_id(cls, data):
-        id = { 'id': data }
+        id = {'id': data}
         query = "SELECT * FROM courses WHERE id = %(id)s;"
         result = connectToMySQL('learn_app').query_db(query, id)
         return cls(result[0])
     @classmethod
     def update(cls, data):
-        query = "UPDATE courses SET title = %(title)s, description = %(description)s WHERE id = %(id)s;"
+        query = "UPDATE courses SET name = %(name)s, description = %(description)s, instructor_id=%(instructor_id)s, category_id=%(categoryy_id)s  WHERE id = %(id)s;"
         return connectToMySQL('learn_app').query_db(query, data)
     @classmethod
     def delete(cls, data):
