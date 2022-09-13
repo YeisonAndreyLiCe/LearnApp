d9613284cf3732144ef8c7abb890fc045b2153df
Juan Camilo Duque Gil <kcamilito@gmail.com>
2022-09-12 22:06:28 -0500

    modify models recodrs, controller record, scrip.sql and __init__.app. install werkzeug

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask, session
 app = Flask(__name__)
 
 app.secret_key = 'super secret key'
+
+app.config['UPLOAD_FOLDER'] = 'flask_app/static/records/'
--- flask_app/controllers/records_controller.py
+++ flask_app/controllers/records_controller.py
@@ -4,6 +4,10 @@ from flask_app.models.user import User
 from flask_app.models.course import Course
 from flask_app.models.category import Category
 from flask_app.models.record import Record
+#Para subir imagenes
+from werkzeug.utils import secure_filename
+import os
+
 
 @app.route('/create_record')
 def create_record():
@@ -13,13 +17,26 @@ def create_record():
 
 @app.route('/insert', methods=['POST'])
 def insert_record():
-    if request.method=='POST':
-        #vamos a validar?
-        errors = Record.validate(request.form)
+    if request.method == 'POST':
+        # vamos a validar?
+        errors = Record.validate_record(request.form)
         if errors:
             return jsonify(errors)
         else:
-            Record.save(request.form)
-            #revisar a donde se va a redireccionar
-            return jsonify({'route':'/courses'})
+            # Almaceno el archivo en una variable
+            file = request.files['course_record']
+            # extraigo el nombre de ese archivo
+            file_name = secure_filename(file.filename)
+            # guardo el archivo en la carpeta del servidor
+            file.save(os.path.join(app.config['UPLOAD_FOLDER']), file_name)
+            # creo un nuevo formulario para enviar a la base de datos
+            form = {
+                'name': request.form['name'],
+                'course_id': request.form['course_id'],
+                'description': request.form['description'],
+                'record': file_name
+            }
+            Record.save(form)
+            # revisar a donde se va a redireccionar
+            return jsonify({'route': '/courses'})
     return redirect('/register_login')
