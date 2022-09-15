3fc67ee01e3f1fa2ac55a65d70eeabe1fe9efbd0
Mariana Mesa <marianamesa4@gmail.com>
2022-09-14 19:20:57 -0500

    comment validation

--- flask_app/models/user_has_courses.py
+++ flask_app/models/user_has_courses.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.course import Course
 
 
 class User_has_Courses:
@@ -18,3 +19,28 @@ class User_has_Courses:
             courses = [cls(course) for course in results]
             return courses
         return False
+    
+    @classmethod
+    def get_enrolled_courses(cls, data):
+        data = {'user_id': data} 
+        query = "SELECT * FROM users_has_courses WHERE user_id = %(user_id)s;"
+        #results = connectToMySQL('learn_app').query_db(query, self.__dict__)
+        results = connectToMySQL('learn_app').query_db(query, data)
+        print('*'*100)
+        print(results)
+        courses = [Course(result) for result in results]
+        return courses
+
+    @classmethod
+    def enroll_course(cls, data):
+        query = "INSERT INTO users_has_courses (user_id, course_id) VALUES (%(user_id)s, %(course_id)s);"
+        return connectToMySQL('learn_app').query_db(query, data)
+    
+    @staticmethod
+    def validate(data):
+        errors={}
+        query='SELECT * FROM users_has_courses WHERE course_id=%(course_id)s and user_id=%(user_id)s'
+        result=connectToMySQL('learn_app').query_db(query,data)
+        if result :
+            errors['enroll_course']='You have already enroll this course'
+        return errors
